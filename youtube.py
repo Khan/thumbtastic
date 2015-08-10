0cac53630b07f0a790936b493ffe1788f9292eca
William Chargin <wchargin@gmail.com>
2015-08-10 16:34:49 -0700

    Implement YouTube design in Blender
    Summary:
    This implements the latest YouTube design specifications.
    Some parts were not specified, so I made those up
    (like the size of the leaf logo),
    but the whole thing is parameterized
    so we can change it easily.
    
    Test Plan:
    Open `youtube.blend`, and, in the console, execute
    
        batch.render_batch(youtube.YouTubeRenderer, "youtube.json")
    
    Since you probably don't want to go to all that trouble,
    here's the output instead:
    
    {F167326}
    {F167327}
    {F167328}

--- youtube.py
+++ youtube.py
@@ -5,10 +5,10 @@ import core
 
 class YouTubeRenderer(core.BaseThumbnailRenderer):
 
-    def __init__(self, input_image="", has_image=True, title_text=""):
+    def __init__(self, input_image=None, title_text="", domain=""):
         self.input_image = input_image
-        self.has_image = has_image
         self.title_text = title_text
+        self.domain = domain
 
     def set_up(self):
         scene = self.get_scene()
@@ -19,29 +19,30 @@ class YouTubeRenderer(core.BaseThumbnailRenderer):
         width = 5.0
         height = width * (720.0 / 1280.0)
 
-        # Offset from the edge of the canvas
-        offset = 0.02 * width
+        # Determine the height at which we want the text to appear.
+        base_line_height = (13.0 / 90) * (height * 2)
+        newline_count = self.title_text.count('\n')
+        multiline_scaling_factor = 0.5 if newline_count > 0 else 1.0
+        nominal_line_height = base_line_height * multiline_scaling_factor
 
-        nominal_text_size = 0.5  # in Blender units
-
-        # The actual text height is considerably smaller than nominal.
+        # Text appears considerably smaller than the nominal size,
+        # so we use this scaling factor to scale it up.
         # Determine the scaling factor experimentally
         # by setting the nominal text size to 1.0
         # and measuring the ascent manually.
         # If the font changes, you may have to re-measure this.
-        actual_text_height = nominal_text_size * 0.5627
+        actual_line_height = nominal_line_height / 0.5627
 
         # Assign properties to the text geometry.
         text_geom = bpy.data.curves["TitleText"]
         text_geom.body = self.title_text.upper()
-        text_geom.size = nominal_text_size
-        self.set_switch(nodes["TextExists"], self.title_text)
+        text_geom.size = actual_line_height
 
         # Position text object appropriately.
+        vertical_offset = newline_count * actual_line_height / 2
         bpy.data.objects["TitleText"].location = (
-            (0, height - offset - actual_text_height, 0))
+            (0, vertical_offset, 0))
 
-        image_valid = self.set_image(nodes["InputImage"], self.input_image)
-        self.set_switch(nodes["ImageExists"], image_valid)
-        if self.has_image and not image_valid:
-            return "Failed to load image"
+        # Set the domain color in the node tree.
+        domain_color = self.get_domain_color(self.domain)
+        nodes["DomainColor"].outputs[0].default_value = domain_color
